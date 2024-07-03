eb69bd0c69019faad24822a53d2be1c67e76578d
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2024-07-03 19:47:51 +0000

    added UsersController and show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "/:username" => "users#show", as: :user
 
 end
